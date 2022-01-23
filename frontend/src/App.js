0ac49144823e923bd473eea23adcae6558bc33bb
AD1121 <sinagh30@gmail.com>
2022-01-23 23:31:50 +0100

    Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,16 @@ import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 const App = () => {
   return (
@@ -14,7 +23,18 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
+            <Route path='/payment' element={<PaymentScreen />} />
+            <Route path='/placeorder' element={<PlaceOrderScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/shipping' element={<ShippingScreen />} />
+            <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/order/:id' element={<OrderScreen />} />
+            <Route path='/cart' element={<CartScreen />}>
+              <Route path='/cart/:id' element={<CartScreen />} />
+            </Route>
           </Routes>
         </Container>
       </main>
